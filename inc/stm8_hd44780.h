2074bf3e93a12c025359ab3716eb0aeaf7ac5d28
Marrek Nožka <nozka@spseol.cz>
2021-11-30 11:31:21 +0100

    ve škole

--- inc/stm8_hd44780.h
+++ inc/stm8_hd44780.h
@@ -67,20 +67,25 @@
 
 // define your own pinout. Do not forgot that driver needs 5V tolerant pins
 #define LCD_RS_PORT     GPIOF
-#define LCD_RW_PORT     GPIOF
-#define LCD_E_PORT      GPIOF
-#define LCD_D4_PORT     GPIOG
-#define LCD_D5_PORT     GPIOG
-#define LCD_D6_PORT     GPIOG
-#define LCD_D7_PORT     GPIOG
-
 #define LCD_RS_PIN  GPIO_PIN_7
+
+#define LCD_RW_PORT     GPIOF
 #define LCD_RW_PIN  GPIO_PIN_6
+
+#define LCD_E_PORT      GPIOF
 #define LCD_E_PIN   GPIO_PIN_5
-#define LCD_D4_PIN  GPIO_PIN_0
-#define LCD_D5_PIN  GPIO_PIN_1
-#define LCD_D6_PIN  GPIO_PIN_2
-#define LCD_D7_PIN  GPIO_PIN_3
+
+#define LCD_D4_PORT     GPIOD
+#define LCD_D4_PIN  GPIO_PIN_6
+
+#define LCD_D5_PORT     GPIOD
+#define LCD_D5_PIN  GPIO_PIN_5
+
+#define LCD_D6_PORT     GPIOE
+#define LCD_D6_PIN  GPIO_PIN_0
+
+#define LCD_D7_PORT     GPIOC
+#define LCD_D7_PIN  GPIO_PIN_1
 
 // define only if powering LCD from GPIO (feature for low power applications)
 #ifdef USE_POWER_PIN
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
-#include "stm8s.h"
 #include "milis.h"
+#include "stm8s.h"
+#include "stm8_hd44780.h"
+#include <stdio.h>
 
 /*#include "delay.h"*/
 /*#include <stdio.h>*/
@@ -9,40 +11,48 @@
 #define _GNU_SOURCE
 
 #define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define LED_ON   GPIO_WriteHigh(LED_PORT, LED_PIN);
-#define LED_OFF  GPIO_WriteLow(LED_PORT, LED_PIN);
-#define LED_FLIP GPIO_WriteReverse(LED_PORT, LED_PIN);
+#define LED_PIN GPIO_PIN_5
+#define LED_ON GPIO_WriteHigh(LED_PORT, LED_PIN)
+#define LED_OFF GPIO_WriteLow(LED_PORT, LED_PIN)
+#define LED_FLIP GPIO_WriteReverse(LED_PORT, LED_PIN)
 
 #define BTN_PORT GPIOE
-#define BTN_PIN  GPIO_PIN_4
-#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
+#define BTN_PIN GPIO_PIN_4
+#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN) == RESET)
 
-
-void init(void)
+void setup(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
+    
+    lcd_init();
+
     init_milis();
 }
-
 
 int main(void)
 {
     uint32_t time = 0;
-    init();
-    /*init_uart();*/
+    uint16_t pocitadlo = 0;
+    char text[16];
+    
+    setup();
+
+    lcd_gotoxy(0,0);
+    lcd_puts("ahoj");
 
     while (1) {
 
         if (milis() - time > 333 && BTN_PUSH) {
-            GPIO_WriteReverse(LED_PORT, LED_PIN);
+            LED_FLIP;
             time = milis();
+            lcd_gotoxy(0,0);
+            sprintf(text, "cislo %d", pocitadlo++);
+            lcd_puts(text);
         }
 
-        /*LED_FLIP; */
-        /*_delay_ms(333);*/
-        /*printf("Funguje to!!!\n");*/
     }
 }
 
